ce6edd27986e60f0ec95aca99267eb4521b949ab
Lucas Leplanois <lucas.leplanois@epita.fr>
2021-03-25 20:37:14 +0100

    lucas
    Signed-off-by: Lucas Leplanois <lucas.leplanois@epita.fr>

--- BookDeployment/BookDeployment/Controllers/BooksController.cs
+++ BookDeployment/BookDeployment/Controllers/BooksController.cs
@@ -1,16 +1,40 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TestApplication.Data;
+using TestApplication.DTO;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using TestApplication.Models;
+using DTO;
 
 namespace BookDeployment.Controllers
 {
-    public class BooksController : Controller
+    [ApiController]
+    [Route("[controller]")]
+    public class BooksController : ControllerBase
     {
-        public IActionResult Index()
+        private readonly Context _context;
+
+        public BooksController(Context context)
         {
-            return View();
+            _context = context;
         }
-    }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<BookDTO>>> GetBooks()
+        {
+            var book = from books in _context.Book
+            join book_descriptions in _context.Book_Description on books.id equals book_descriptions.book_id
+            select new BookDTO
+            {
+                Book_id = books.id,
+                Book_price = books.price,
+                ISBN = books.isbn,
+                Book_name = book_descriptions.book_name,
+                Book_description = book_descriptions.book_description
+            };
+
+            return await book.ToListAsync();
+        }
 }
